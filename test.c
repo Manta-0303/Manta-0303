a6b2e6180b69f1934d2bc9502581162d370ca85d
manta <rltjs123456@gmail.com>
2022-03-20 21:45:45 +0900

    first commit

--- test.c
+++ test.c
@@ -1,54 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: manta <manta@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/20 20:14:29 by manta             #+#    #+#             */
+/*   Updated: 2022/03/20 21:43:03 by manta            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-int g_len;
-char g_buf[10];
-
-void    rec(int prev, int n)
+int	main(void)
 {
-	int i;
+	char	c1;
+	char	c2;
+	char	c3;
 
-    if (n == g_len)
-    {
-        write(1, g_buf, g_len);
-        write(1, ", ", 2);
-        return ;
-    }
-    if (prev == 9)
-        return ;
-    i = prev;
-    while (++i <= 10 - g_len + n)
-    {
-        g_buf[n] = '0' + i;
-		rec(i, n + 1);
-    }
-}
-
-void    print_last(int i)
-{
-    char c;
-
-    while (i <= 9)
-    {
-        c = '0' + i++;
-        write(1, &c, 1);
-    }
-}
-
-void    ft_print_combn(int n)
-{
-    int i;
-
-    i = 0;
-    g_len = n;
-    while (i < 10 - n)
-    {
-        g_buf[0] = '0' + i;
-        rec(i++, 1);
-    }
-    print_last(i);
-}
-
-int    main(void)
-{
-    ft_print_combn (3);
+	c1 = '0';
+	c2 = '0';
+	c3 = '0';
+	while (c1 <= '9')
+	{
+		while (c2 <= '9')
+		{
+			while (c3 <= '9')
+			{
+				write(1, &c1, 1);
+				write(1, &c2, 1);
+				write(1, &c3, 1);
+				write(1, "\n", 1);
+				c3++;
+			}
+			c3 = '0';
+			c2++;
+		}
+		c2 = '0';
+		c1++;
+	}
 }
